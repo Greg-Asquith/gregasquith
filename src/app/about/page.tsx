40372367dbfd511b09876b0b44f35c3932a53017
Greg-Asquith <greg@gregasquith.com>
2023-03-05 22:31:03 +0000

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,13 @@
 
 export default function About() {
   return (
-    <div className="my-2">
-      About Greg
-    </div>
+    <>
+      <div className="my-2">
+        About Greg
+      </div>
+      <div className="my-2">
+        About This Site
+      </div>
+    </>
   );
 }
