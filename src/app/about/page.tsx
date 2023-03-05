e990d60994763290f0594b32abbb42562172f7a3
Greg-Asquith <greg@ad-lib.io>
2023-03-05 22:25:53 +0000

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,8 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Greg Asquith | About',
-  description: 'Greg Asquith | Technical consulting and solution creation',
-};
 
 export default function About() {
   return (
     <div className="my-2">
-      About gregasquith.com
+      About Greg
     </div>
   );
 }
